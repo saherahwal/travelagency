f498c769ea944c945905bea84fc8d97061cf107a
Saher Ahwal <saher.ahwal@gmail.com>
2016-11-18 04:14:50 +0000

    Apache configuration for running escanza in production on web port 80 and 443

--- ota/ota/wsgi.py
+++ ota/ota/wsgi.py
@@ -8,6 +8,13 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append('/home/escanzaadmin/escanza/travelagency/ota')
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ota.settings")
+
+os.environ.setdefault("PYTHON_EGG_CACHE", "/home/escanzaadmin/escanza/travelagency/ota/egg_cache")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ota.settings")
 
 from django.core.wsgi import get_wsgi_application
